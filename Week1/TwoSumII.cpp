a70c49cb1e55e613b04b086ba990e81e27f22e83
Xhovani Mali <xhovani03@live.com>
2024-10-01 21:25:02 -0400

    Week 1 Problems

---
+++ Week1/TwoSumII.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right)
+        {
+            int curSum = numbers[left] + numbers[right];
+
+            if (curSum > target)
+            {
+                right--;
+            }
+            else if (curSum < target)
+            {
+                left++;
+            }
+            else
+            {
+
+                return {left + 1, right + 1};
+            }
+        }
+
+        // In case no solution is found (although the problem guarantees there is one)
+        return {};
+    }
+};
