829685b8b18cb9d61e0d80ff4453c792558fed15
Xhovani Mali <xhovani03@live.com>
2024-11-05 12:10:03 -0500

    added lowest common ancestory of binary tree solution

--- Week5/LowestCommonAncestorofaBinaryTree.cpp
+++ Week5/LowestCommonAncestorofaBinaryTree.cpp
@@ -12,5 +12,23 @@ class Solution
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
+        // Base Case (If the treenode is not connected to anything return root)
+        if (!root || root == p || root == q)
+        {
+            return root;
+        }
+
+        // Recursive calls for left and right subtrees
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        // If both left and right are non-null, root is the LCA
+        if (left && right)
+        {
+            return root;
+        }
+
+        // Other return non-null child
+        return left ? left : right;
     }
 };
